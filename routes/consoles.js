75289d1189bbb20ce2f20161ae87b28d2c9e8f98
SaraTamurejo <sara.tamurejo.7e8@itb.cat>
2025-10-18 18:05:41 +0200

    hacer cruds

--- routes/consoles.js
+++ routes/consoles.js
@@ -9,12 +9,65 @@ const writeData = (data) => fs.writeFileSync('./db/db.json', JSON.stringify(data
 
 //ver -- get todo
 
+router.get('/',(req,res) => {
+
+    const data = readData();
+    res.render ("consolas", data);
+
+
+
+} )
+
 //ver -- get una
+router.get('/:id',(req,res) => {
+
+    const data = readData();
+    const consola = data.consolas.find(p => p.id === parseInt(req.params.id));
+    if (!consola) return res.status(404).send('La consola que buscas no existe');
+
+    res.render ("detalleConsola", juego);
+
+} )
+
 
 //crear --post
 
+router.post('/',(req,res) => {
+
+    const data = readData();
+    const { imagen, nombre, valoracion} = req.body;
+    if (!imagen || !nombre|| !valoracion) return res.status(400).send('Necesitas rellenar todos los campos');
+    const nuevaConsola = { id: data.consolas.length + 1, imagen, nombre, valoracion };
+    data.consolas.push(nuevaConsola);
+    writeData(data);
+    res.json(nuevaConsola);
+
+})
+
 //modificar -- put
 
+router.put('/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const consolaIndex = data.consolas.findIndex(p => p.id === id);
+    if (consolaIndex === -1) return res.status(404).send('Consola no encontrado');
+    data.consolas[consolaIndex] = { ...data.consolas[consolaIndex], ...req.body };
+    writeData(data);
+    res.json({ message: 'Consola actualizada!' });
+});
+
 //eliminar -- delete
+
+router.delete('/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const consolaIndex = data.consolas.findIndex(p => p.id === id);
+    if (consolaIndex === -1) return res.status(404).send('Consola no encontrado');
+    data.consolas.splice(consolaIndex, 1);
+    writeData(data);
+    res.json({ message: 'Consola borrada correctamente' });
+});
+
+
 
 export default router;
--- routes/games.js
+++ routes/games.js
@@ -9,7 +9,7 @@ const writeData = (data) => fs.writeFileSync('./db/db.json', JSON.stringify(data
 
 //ver -- get todo
 
-router.get('/games',(req,res) => {
+router.get('/',(req,res) => {
 
     const data = readData();
     res.render ("juegos", data);
@@ -19,11 +19,55 @@ router.get('/games',(req,res) => {
 } )
 
 //ver -- get una
+router.get('/:id',(req,res) => {
+
+    const data = readData();
+    const juego = data.juegos.find(p => p.id === parseInt(req.params.id));
+    if (!juego) return res.status(404).send('No existe el juego que buscas');
+
+    res.render ("detalleJuego", juego);
+
+} )
+
 
 //crear --post
 
+router.post('/',(req,res) => {
+
+    const data = readData();
+    const { imagen, titulo, valoracion} = req.body;
+    if (!imagen || !titulo|| !valoracion) return res.status(400).send('Necesitas rellenar todos los campos');
+    const nuevoJuego = { id: data.juegos.length + 1, imagen, titulo, valoracion };
+    data.juegos.push(nuevoJuego);
+    writeData(data);
+    res.json(nuevoJuego);
+
+})
+
 //modificar -- put
 
+router.put('/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const juegoIndex = data.juegos.findIndex(p => p.id === id);
+    if (juegoIndex === -1) return res.status(404).send('Juego no encontrado');
+    data.juegos[juegoIndex] = { ...data.juegos[juegoIndex], ...req.body };
+    writeData(data);
+    res.json({ message: 'Juego actualizado!' });
+});
+
 //eliminar -- delete
+
+router.delete('/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const juegoIndex = data.juegos.findIndex(p => p.id === id);
+    if (juegoIndex === -1) return res.status(404).send('Juego no encontrado');
+    data.juegos.splice(juegoIndex, 1);
+    writeData(data);
+    res.json({ message: 'Juego borrado correctamente' });
+});
+
+
 
 export default router;
